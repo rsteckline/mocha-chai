2eb4d5bb5549eb3124394c20254727087127b2ac
Bobby Steckline <rsteckline@gmail.com>
2024-10-04 14:51:04 -0400

    Nest function tests for organization

--- test/appTest.js
+++ test/appTest.js
@@ -8,24 +8,30 @@ sayHelloResult = app.sayHello();
 addNumbersResult = app.addNumbers(5, 5);
 
 describe('App', function () {
-    it('sayHello should return hello', function () {
-        //let result = app.sayHello();
-        assert.equal(sayHelloResult, 'hello');
-    })
+    describe('sayHello()', function () {
 
-    it('sayHello should return type string', function () {
-        //let result = app.sayHello();
-        assert.typeOf(sayHelloResult, 'string');
+        it('sayHello should return hello', function () {
+            //let result = app.sayHello();
+            assert.equal(sayHelloResult, 'hello');
+        })
+
+        it('sayHello should return type string', function () {
+            //let result = app.sayHello();
+            assert.typeOf(sayHelloResult, 'string');
+        });
     });
 
-    it('addNumbers should be above 5', function () {
-        //let result = app.addNumbers(5, 5);
-        assert.isAbove(addNumbersResult, 5)
-    })
+    describe('addNumbers()', function () {
+
+        it('addNumbers should be above 5', function () {
+            //let result = app.addNumbers(5, 5);
+            assert.isAbove(addNumbersResult, 5)
+        })
 
 
-    it('addNumbers should return type number', function () {
-        //let result = app.addNumbers(5, 5);
-        assert.typeOf(addNumbersResult, 'number');
+        it('addNumbers should return type number', function () {
+            //let result = app.addNumbers(5, 5);
+            assert.typeOf(addNumbersResult, 'number');
+        });
     });
 });

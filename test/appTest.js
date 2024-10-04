dd0a073791a286831bfd839c6f3d8d2bc922a365
Bobby Steckline <rsteckline@gmail.com>
2024-10-04 14:48:59 -0400

    Refactor imports for less requires

--- test/appTest.js
+++ test/appTest.js
@@ -1,27 +1,31 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-//const app = require('../app')
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumbers;
+const app = require('../app')
+
+//Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
 
 describe('App', function () {
     it('sayHello should return hello', function () {
-        let result = sayHello();
-        assert.equal(result, 'hello');
+        //let result = app.sayHello();
+        assert.equal(sayHelloResult, 'hello');
     })
 
     it('sayHello should return type string', function () {
-        let result = sayHello();
-        assert.typeOf(result, 'string');
+        //let result = app.sayHello();
+        assert.typeOf(sayHelloResult, 'string');
     });
 
     it('addNumbers should be above 5', function () {
-        let result = addNumbers(5, 5);
-        assert.isAbove(result, 5)
+        //let result = app.addNumbers(5, 5);
+        assert.isAbove(addNumbersResult, 5)
     })
 
 
     it('addNumbers should return type number', function () {
-        let result = addNumbers(5, 5);
-        assert.typeOf(result, 'number');
+        //let result = app.addNumbers(5, 5);
+        assert.typeOf(addNumbersResult, 'number');
     });
 });

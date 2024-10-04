0f69d29f6b23f6a314b6cad9745f25e224ce218e
Bobby Steckline <rsteckline@gmail.com>
2024-10-04 14:38:01 -0400

    Add test for return type

--- test/appTest.js
+++ test/appTest.js
@@ -3,9 +3,13 @@ const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
 
 describe('App', function () {
-    it('app should return hello', function () {
+    it('sayHello should return hello', function () {
         let result = sayHello();
         assert.equal(result, 'hello');
-
     })
+
+    it('sayHello should return type string', function(){
+        let result = sayHello();
+        assert.typeOf(result, 'string');
+    });
 });
